494d86f5558da8fdbf5e6c2333dd411bb1eabd59
veffhz <veffhz@example.com>
2020-06-22 22:59:59 +0300

    Fixed review remarks (url)

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -4,9 +4,9 @@ from tours.views import MainView, DepartureView, TourView
 from tours.views import custom_handler404, custom_handler500
 
 urlpatterns = [
-    path('', MainView.as_view()),
-    path('departure/<str:departure>/', DepartureView.as_view()),
-    path('tour/<int:tour_id>/', TourView.as_view())
+    path('', MainView.as_view(), name='main'),
+    path('departure/<str:departure>/', DepartureView.as_view(), name='departure'),
+    path('tour/<int:tour_id>/', TourView.as_view(), name='tour')
 ]
 
 handler404 = custom_handler404
